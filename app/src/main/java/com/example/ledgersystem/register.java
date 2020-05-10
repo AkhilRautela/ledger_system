e8ec8d329cf1a0abe194747b53528f89d6b13e69
adityakrmishra1 <mishraa878878@gmail.com>
2020-05-10 14:03:22 +0530

    modifying registation  activity

--- app/src/main/java/com/example/ledgersystem/register.java
+++ app/src/main/java/com/example/ledgersystem/register.java
@@ -2,7 +2,7 @@ package com.example.ledgersystem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.ProgressBar;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -23,6 +23,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class register extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    ProgressBar progressBar;
     EditText email,pass;
     EditText phone,username;
     FirebaseDatabase fb= FirebaseDatabase.getInstance();
@@ -41,29 +42,37 @@ public class register extends AppCompatActivity {
         v.startAnimation(buttonClick);
         Vibrator vv = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
         vv.vibrate(100);
+        if(username.getText().toString().equals("")||phone.getText().toString().equals("")||email.getText().toString().equals(""))
+        {
+            Toast.makeText(getApplicationContext(),"NULL CREDENTISLD :(",Toast.LENGTH_SHORT).show();
 
+        }
+        else
+        {
 
-        mAuth.createUserWithEmailAndPassword(email.getText().toString(),pass.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
+            mAuth.createUserWithEmailAndPassword(email.getText().toString(),pass.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
 
-                if(task.isSuccessful()){
-                   Toast.makeText(getApplicationContext(),"Successfully Registered",Toast.LENGTH_SHORT).show();
-                   String na=username.getText().toString();
-                   DatabaseReference dref=fb.getReference("Users");
-                   dref.child(na).child("Phone_number").setValue(phone.getText().toString());
-                   dref.child(na).child("E_mail").setValue(email.getText().toString());
-                   DatabaseReference dd=fb.getReference("Phonenumbers");
-                   dd.child(phone.getText().toString()).setValue(username.getText().toString());
-                   Intent i = new Intent(getApplicationContext(),login.class);
-                   startActivity(i);
-                   finishAffinity();
-               }
-               else{
-                   Toast.makeText(getApplicationContext(),"ERROR IN CREATING ACCOUNT",Toast.LENGTH_SHORT).show();
-               }
-            }
-        });
+                    if(task.isSuccessful()){
+                        Toast.makeText(getApplicationContext(),"Successfully Registered",Toast.LENGTH_SHORT).show();
+                        String na=username.getText().toString();
+                        DatabaseReference dref=fb.getReference("Users");
+                        dref.child(na).child("Phone_number").setValue(phone.getText().toString());
+                        dref.child(na).child("E_mail").setValue(email.getText().toString());
+                        DatabaseReference dd=fb.getReference("Phonenumbers");
+                        dd.child(phone.getText().toString()).setValue(username.getText().toString());
+                        Intent i = new Intent(getApplicationContext(),login.class);
+                        startActivity(i);
 
-    }
+                        finishAffinity();
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(),"ERROR IN CREATING ACCOUNT",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+        }
+        }
 }
