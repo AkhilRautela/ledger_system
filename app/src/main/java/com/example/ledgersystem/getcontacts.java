8d683d2156435886763bf5ccd27087b30b099e6e
Akhil_Rautela <akhilrautela36@gmail.com>
2020-05-10 08:41:04 +0530

    FEW TWEAKS

--- app/src/main/java/com/example/ledgersystem/getcontacts.java
+++ app/src/main/java/com/example/ledgersystem/getcontacts.java
@@ -27,7 +27,7 @@ public class getcontacts {
         Cursor phones = s.getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
         while (phones.moveToNext()) {
             String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-            pnumber[i] = phoneNumber;
+            pnumber[i] = phoneNumber.replaceAll("\\D", "");
             i++;
         }
 
---
+++ app/src/main/java/com/example/ledgersystem/multicontactselector_adapter.java
@@ -0,0 +1,28 @@
+package com.example.ledgersystem;
+
+import android.view.ViewGroup;
+import android.widget.Adapter;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+/*public class multicontactselector_adapter extends RecyclerView.Adapter<multicontactselector_adapter.ViewHolder> {
+
+
+    @NonNull
+    @Override
+    public multicontactselector_adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull multicontactselector_adapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
+*/
